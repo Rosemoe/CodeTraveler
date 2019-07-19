9d69cabe836884e7372c19157070e9d18b969f49
Rose2073 <2073412493@qq.com>
2019-07-19 16:53:16 +0800

    Project created

---
+++ CodeTraveler/app/build/gen/com/rose/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** 自动生成的文件，不要修改 */
+package com.rose.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = false;
+}
---
+++ CodeTraveler/app/src/main/java/com/rose/android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rose.android;
+
+import android.app.*;
+import android.os.*;
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
